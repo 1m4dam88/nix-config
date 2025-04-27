fb3aafbcb2bf833224fb8a7f5ad4d9acd03326cd
1m4dam88 <171095855+1m4dam88@users.noreply.github.com>
2025-04-28 00:44:59 +0100

    fix river?

--- modules/home/light-gui/river.nix
+++ modules/home/light-gui/river.nix
@@ -5,8 +5,14 @@
     xwayland.enable = true;
     settings = {
       set-repeat = "50 300";
+      focus-follows-cursor = "always";
       default-layout = "rivertile";
+      output-layout = "rivertile";
+      attach-mode = "bottom";
       border-width = 2;
+      spawn = [
+        "rivertile"
+      ];
       map = {
         normal = {
           "Super" = {
@@ -23,21 +29,16 @@
             Space = "toggle-float";
           };
         };
-        # (lib.attrsets.mapAttrs
-        #  (_mod: value: lib.attrsets.genAttrs (lib.lists.forEach (lib.lists.range 1 9) (num: toString num))
-        #    (tag: value + builtins.replaceStrings [ "TAG" ] [ tag ] " $((1 << (TAG - 1)))"))
-        #  {
-        #    "Super" = "set-focused-tags";
-        #    "Super+Shift" = "set-view-tags";
-        #    "Super+Control" = "toggle-focused-tags";
-        #    "Super+Shift+Control" = "toggle-view-tags";
-        #  })
-        # ];
-        #map-pointer.normal."Super" = {
-        #"BTN_LEFT" = "move-view";
-        #"BTN_RIGHT" = "resize-view";
-      };
-
+      }
+        (lib.attrsets.mapAttrs
+          (_mod: value: lib.attrsets.genAttrs (lib.lists.forEach (lib.lists.range 1 9) (num: toString num))
+            (tag: value + builtins.replaceStrings [ "TAG" ] [ tag ] " $((1 << (TAG - 1)))"))
+          {
+            "Super" = "set-focused-tags";
+            "Super+Shift" = "set-view-tags";
+            "Super+Control" = "toggle-focused-tags";
+            "Super+Shift+Control" = "toggle-view-tags";
+          })
     };
   };
 }
