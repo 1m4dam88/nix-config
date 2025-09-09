038a2d95136386138f23fc795ee88e37a635930c
1m4dam88 <171095855+1m4dam88@users.noreply.github.com>
2025-09-09 03:07:16 +0100

    update desktop

--- modules/home/gui/walker.nix
+++ modules/home/gui/walker.nix
@@ -1,4 +1,16 @@
 { config, inputs, pkgs, host, ... }:
 {
-  home.packages = with pkgs; [ walker ];
+  imports = [inputs.walker.homeManagerModules.default];
+  programs.walker = {
+    enable = true;
+    runAsService = true;
+
+    config = {
+      placeholders."default".input = "Searx";
+      providers.prefixes = [
+        { provider = "websearch"; prefix = "+";}
+      ];
+      keybinds.quick_activate = ["F1" "F2" "F3"];
+    };
+  };
 }
