19c18ff3e1cb0c36dbc05d365d28a6a9ca6fc7f9
1m4dam88 <171095855+1m4dam88@users.noreply.github.com>
2025-09-13 03:26:09 +0100

    update desktop and readme

--- modules/home/gui/foot.nix
+++ modules/home/gui/foot.nix
@@ -2,9 +2,21 @@
 {
   programs.foot = {
    enable = true;
+   server.enable = true;
+   enableFishIntegration.enable = true;
    settings = {
      main = {
       pad = "0x0";
+     };
+     key-bindings = {
+       scrollback-up-page = "Mod1+k";
+       scrollback-down-page = "Mod1+j";
+       search-start = "Mod1+/";
+       clipboard-copy = "Mod1+c";
+       clipboard-paste = "Mod1+v";
+       font-increase = "Mod1+Shift+k";
+       font-decrease = "Mod1+Shift+j";
+       font-reset = "Mod1+Shift+l"
      };
    };
   };
